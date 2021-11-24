dd477149e3e2e7904b475e01f725acca4105c60f
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-24 11:59:41 -0500

    :sparkles: async await
    I dont like concurrency In rust yet

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use clap::App;
+use futures::executor::block_on;
 use std::{fs, io, path};
 use walkdir::WalkDir;
 
-fn to_del() -> Vec<&'static str> {
+fn to_delete() -> Vec<&'static str> {
     vec![
         "node_modules",
         "__pycache__",
@@ -12,7 +14,7 @@ fn to_del() -> Vec<&'static str> {
     ]
 }
 
-fn delete(entry: &path::Path) -> io::Result<()> {
+async fn delete(entry: &path::Path) -> io::Result<()> {
     if entry.is_file() {
         fs::remove_file(entry)?;
     } else {
@@ -24,12 +26,25 @@ fn delete(entry: &path::Path) -> io::Result<()> {
     Ok(())
 }
 
-fn main() -> io::Result<()> {
-    for dir in to_del().iter() {
+async fn async_main() -> io::Result<()> {
+    for dir in to_delete().iter() {
         for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
-            delete(entry.path())?;
+            delete(entry.path()).await?;
         }
     }
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    App::new("moni")
+        .bin_name("moni")
+        .version("v0.1.0")
+        .author("UltiRequiem <https://github.com/UltiRequiem>")
+        .about("Delete directories to free up disk space.")
+        .get_matches();
+
+    block_on(async_main())?;
 
     Ok(())
 }
